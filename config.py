eeb0983141f3f398c2fdf8f36e8aabbd123f89a3
thesouleddev <sasidharanofficial@gmail.com>
2022-10-19 18:36:42 +0100

    added config and import cloud datastore

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+ROOT = os.path.dirname(__file__)
+
+app = None
--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from flask import Flask
+import config
 
 def main():
     global app
     app = Flask(__name__)
+    config.app = app
+    print(config.ROOT)
+
+    @app.route("/test")
+    def hello():
+        return f"Hello!"
+
     return app
 
 if __name__ == '__main__':
     main()
-    print("hh")
-    app.run()
+    app.run(debug=True, port=8080)
